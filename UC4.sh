47983d368b999cb59b233b3578f47c6ffefac304
keerthika-m <mkeerthika0901@gmail.com>
2023-02-28 18:48:53 +0530

    [keerthana] add : using shell script solve wage employee problem

---
+++ UC4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#variables
+random_check=$(( RANDOM % 3 )) # 0/1/2
+
+is_present_full_time=1
+is_present_part_time=2
+wage_per_hour=20
+
+#selection
+case $random_check in
+
+$is_present_full_time) emp_hours=8
+echo " Employee is present for full time"
+;;
+
+$is_present_part_time) emp_hours=4
+echo " Employee is present for part time"
+;;
+*) emp_hours=0
+echo " Employee is absent"
+;;
+esac
+
+salary=$(( $emp_hours * $wage_per_hour))
+echo " Salary of an employee is $salary "
